bce0156bd2606bb7679ebd3eaa87b8b798f1048b
Matthew Chon <matthew.chon13@qmail.cuny.edu>
2021-02-14 15:13:00 -0500

    Added specs and changed filename (MatthewChon)

--- Assignment1/MatthewChonA1.java
+++ Assignment1/MatthewChonA1.java
@@ -1,3 +1,5 @@
+package Assignment1;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,7 +20,6 @@ class DistributionLinkedList {
         }
         public Double data() { return data; }
         public Node next() { return next; }
-        public void setData(double newdata) { data = newdata; }
         public void setNext(Node newnode) { next = newnode; }
         public String toString() {
             StringBuilder node = new StringBuilder();
@@ -50,19 +51,18 @@ class DistributionLinkedList {
     }
     public void generate_node() {
         double probability = Math.random();
-        double number = 0.0; 
-        number = Math.random();
+        double number = Math.random();
 
         if (probability <= TEN_PROB_BOUND) {
-            number *= 10.00;
+            number *= 10;
             insert(new Node(number, null));
         }
         else if (probability <= HUND_PROB_BOUND) {
-            number *= 100.00;
+            number *= 100;
             insert(new Node(number, null));
         }
         else if (probability <= THOUS_PROB_BOUND) {
-            number *= 1000.00;
+            number *= 1000;
             insert(new Node(number, null));
         }
         increment_count(number);
@@ -79,7 +79,7 @@ class DistributionLinkedList {
         }
     }
     public double generate_percent(Double occurence) {
-        return (occurence / size)* 100.00;
+        return (occurence / size)* 100;
     }
     public void printList(File out) {
         try (FileWriter output = new FileWriter(out)) {
@@ -88,15 +88,15 @@ class DistributionLinkedList {
                 output.append(ptr.toString());
                 ptr = ptr.next();
             }
-            System.out.println("The frequency of values 0 <= x <= 10 is :" + generate_percent(ten));
-            System.out.println("The frequency of values 10 < x <= 100 is :" + generate_percent(hundred));
-            System.out.println("The frequency of values 100 < x <= 1000 is :" + generate_percent(thousand));
+            System.out.println("The frequency of values 0 <= x <= 10 is : " + generate_percent(ten) + "%.");
+            System.out.println("The frequency of values 10 < x <= 100 is : " + generate_percent(hundred) + "%.");
+            System.out.println("The frequency of values 100 < x <= 1000 is : " + generate_percent(thousand) + "%.");
         } catch(IOException e) {
             e.printStackTrace();
         }
     }
 }
-public class MatthewChon {
+public class MatthewChonA1 {
     public static void main(String [] args){
         int limit = 50000;
         String filename = "MatthewChon.txt";
