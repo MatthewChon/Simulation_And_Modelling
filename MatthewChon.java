32e396be54ac5cb505d3293c6afc519e3de89086
Matthew Chon <matthew.chon13@qmail.cuny.edu>
2021-02-14 14:58:58 -0500

    Complete Assignment1

--- MatthewChon.java
+++ MatthewChon.java
@@ -4,78 +4,94 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 class DistributionLinkedList {
-    private final Double TEN_PROB_BOUND = .3,
+    private final double TEN_PROB_BOUND = .3,
                          HUND_PROB_BOUND = .9,
                          THOUS_PROB_BOUND = 1.0;
 
     //---------------- Node class --------------------
     private class Node {
-        private Float data;
+        private double data;
         private Node next;
 
-        public Node(Float d, Node n) {
+        public Node(double d, Node n) {
             data = d;
             next = n;
         }
-        public Float data() { return data; }
+        public Double data() { return data; }
         public Node next() { return next; }
-        public void setData(Float newdata) { data = newdata; }
+        public void setData(double newdata) { data = newdata; }
         public void setNext(Node newnode) { next = newnode; }
-        public void printNode(File out) throws IOException {
+        public String toString() {
             StringBuilder node = new StringBuilder();
             node.append("( " + data + " ) -> ");
-            try {
-                FileWriter write = new FileWriter(out);
-                write.append(node);
-                write.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            return node.toString();
         }
     } //----------------------------------------------
     private Node head;
     private int size;
+    private double ten = 0.0,
+                   hundred = 0.0,
+                   thousand = 0.0;
 
     public DistributionLinkedList() {
-        head = null;
+        head = new Node(-1.0, null);
         size = 0;
     }
     public int size() { return size; }
     public boolean empty() { return size == 0; }
-    private void insert(Node node) {
-        if (empty()) {
-            head = node;
-        }
-        else {   
-            Node place = head;
+    private void insert(Node node) { 
+        Node place = head;
 
-            while (place.next() != null && place.next().data() > node.data()) {
-                place = place.next();
-            }
-            node.setNext(place.next());
-            place.setNext(node);
+        while (place.next() != null && place.next().data() < node.data()) {
+            place = place.next();
         }
+        node.setNext(place.next());
+        place.setNext(node);
         size++;
     }
     public void generate_node() {
-        Random rand = new Random();
-        Random num_rand = new Random();
+        double probability = Math.random();
+        double number = 0.0; 
+        number = Math.random();
 
-        Double probability = rand.nextDouble();
-        Float number = num_rand.nextFloat();
-
-        /**
-         * TO BE IMPLEMENTED
-         */
+        if (probability <= TEN_PROB_BOUND) {
+            number *= 10.00;
+            insert(new Node(number, null));
+        }
+        else if (probability <= HUND_PROB_BOUND) {
+            number *= 100.00;
+            insert(new Node(number, null));
+        }
+        else if (probability <= THOUS_PROB_BOUND) {
+            number *= 1000.00;
+            insert(new Node(number, null));
+        }
+        increment_count(number);
     }
-    public Float generate_percent(Float occurence) {
-        return occurence / size * 100;
+    public void increment_count(Double number) {
+        if (number <= 10) {
+            ten++;
+        }
+        else if (number <= 100) {
+            hundred++;
+        }
+        else if (number <= 1000) {
+            thousand++;
+        }
+    }
+    public double generate_percent(Double occurence) {
+        return (occurence / size)* 100.00;
     }
     public void printList(File out) {
         try (FileWriter output = new FileWriter(out)) {
-            System.out.println("The frequency of values 0 <= x <= 10 is :");
-            System.out.println("The frequency of values 10 < x <= 100 is :");
-            System.out.println("The frequency of values 100 < x <= 1000 is :");
+            Node ptr = head.next();
+            while(ptr != null) {
+                output.append(ptr.toString());
+                ptr = ptr.next();
+            }
+            System.out.println("The frequency of values 0 <= x <= 10 is :" + generate_percent(ten));
+            System.out.println("The frequency of values 10 < x <= 100 is :" + generate_percent(hundred));
+            System.out.println("The frequency of values 100 < x <= 1000 is :" + generate_percent(thousand));
         } catch(IOException e) {
             e.printStackTrace();
         }
@@ -83,10 +99,13 @@ class DistributionLinkedList {
 }
 public class MatthewChon {
     public static void main(String [] args){
+        int limit = 50000;
         String filename = "MatthewChon.txt";
         File outFile = new File(filename);
         DistributionLinkedList list = new DistributionLinkedList();
-        list.generate_node();
+        for (int i = 0; i < limit; ++i) {
+            list.generate_node();
+        }
         list.printList(outFile);
     }
 }
